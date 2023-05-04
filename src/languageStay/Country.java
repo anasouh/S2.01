dbd0a84f81071617854bdf2121858b9d8fc12011
Rayane BELGUEBLI <rayane.belguebli.etu@if06.iut-infobio.priv.univ-lille1.fr>
2023-05-04 09:13:41 +0200

    test pull

--- src/languageStay/Country.java
+++ src/languageStay/Country.java
@@ -1,4 +1,10 @@
 package languageStay;
+
+/**
+ * <strong>Liste les pays participants à ce programme d'échange </strong>
+ * @author Desmee Nathan, Ouhdda Anas, Belguebli Rayane
+ */
+
 public enum Country {
     FRANCE,
     ITALY,
--- src/languageStay/Teenager.java
+++ src/languageStay/Teenager.java
@@ -1,4 +1,10 @@
 package languageStay;
+
+/**
+ * <strong>permet de créer un objet Teenager </strong>
+ * @author Desmee Nathan, Ouhdda Anas, Belguebli Rayane
+ */
+
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -18,6 +24,14 @@ public class Teenager{
 
     TreeMap<Sejour, Teenager> history = new TreeMap<Sejour, Teenager>();
 
+
+    /**
+     * Crée un Teenager complètement spécifiée
+     * @param name nom du Teenager
+     * @param firstname prénom du Teenager
+     * @param birthday date d'anniversaire du Teenager
+     * @param country pays du Teenager
+     */
     public Teenager(String name, String firstname, LocalDate birthday, Country country){
         this.name = name;
         this.firstname = firstname;
@@ -27,6 +41,10 @@ public class Teenager{
         Teenager.compteur ++;
     }
 
+    /**
+     * Vérifie la compatibilité entre 2 Teenagers
+     * @param teenager le teenager pour la vérifier la compatibilité
+     */
     public boolean compatibleWithGuest(Teenager teenager){
         if(this.criterionEquals("HOST_HAS_ANIMAL", "yes") 
         && teenager.criterionEquals("GUEST_ANIMAL_ALLERGY", "yes")){
@@ -34,8 +52,10 @@ public class Teenager{
         }
         else if(!requierments.get("HOST_FOOD").allIn(teenager.requierments.get("GUEST_FOOD").getValue()) ){
             return false;
-        }
-        else if(this.country == Country.FRANCE || teenager.getCountry() == Country.FRANCE){
+        }    /**
+        * Renvoie true si le teenager n'a pas d'habitude l'alimentaire contraire à celui donné en paramètre 
+        * @param necessary un Teenager
+        */FRANCE || teenager.getCountry() == Country.FRANCE){
             if(!requierments.get("HOBBIES").isIn(teenager.requierments.get("HOBBIES").getValue())){
                 return false;
             }
@@ -47,6 +67,10 @@ public class Teenager{
         return true;
     }
 
+
+    /**
+     * Vérifie si les valeurs des critères est valide et si oui les supprime
+     */
     public void purgeInvalidRequierement(){
         if(requierments.get("HOST_HAS_ANIMAL").equals("yes") && requierments.get("GUEST_ANIMAL_ALLERGY").equals("yes")){
             requierments.remove("HOST_HAS_ANIMAL");
@@ -63,30 +87,57 @@ public class Teenager{
         }
     }
 
+    /**
+     * Ajoute un critère donné en paramètre
+     * @param label nom du critère
+     * @param value valeur du critère
+     */
     public void addCriterion(CriterionName label, String value){
         Criterion criterion =  new Criterion(label, value);
         requierments.put(label.name(), criterion);
     }
 
+
+    /**
+     * Renvoie la valeur d'un critère
+     * @param label nom du critère 
+     */
     public String getCriterion(CriterionName label){
         Criterion res = requierments.get(label.name());
         if (res != null) return res.getValue();
         return null;
     }
 
+    /**
+     * Renvoie le pays du Teenager
+     */
     public Country getCountry() {
         return country;
     }
     
+    /**
+     * Renvoie le nombre de critère d'un Teenager
+     */
     public int getNbCriterion(){
         return this.requierments.size();
     }
 
+    /**
+     * Ajoute les séjours précédents
+     * @param annee l'année du séjour
+     * @param pays le pays dans lequel le séjour a été fait
+     * @param etudiant le Teenager ayant fait le séjour
+     */
     public void addHistory(int annee, Country pays, Teenager etudiant){
         Sejour s = new Sejour(annee, pays);
         this.history.put(s, etudiant);
     }
 
+    /**
+     * Renvoie true si le critère est égal à celui donné en paramètre
+     * @param nameCriterion nom du critère
+     * @param value valeur du critère
+     */
     public boolean criterionEquals(String nameCriterion, String value){
         if(!this.requierments.containsKey(nameCriterion)){
             return false;
@@ -94,6 +145,10 @@ public class Teenager{
         return this.requierments.get(nameCriterion).equals(value);
     }
 
+    /**
+     * Renvoie true si le teenager n'a pas d'habitude l'alimentaire contraire à celui donné en paramètre 
+     * @param teen un Teenager
+     */
     public boolean peutNourrir(Teenager teen){
         if(!teen.requierments.containsKey("GUEST_FOOD") && !this.requierments.containsKey("HOST_FOOD")){
             return false;
@@ -102,7 +157,10 @@ public class Teenager{
         ArrayList<String> give = new ArrayList<>();
         if(!teen.requierments.get("GUEST_FOOD").equals("")){
             for (String s : teen.requierments.get("GUEST_FOOD").getValue().split(",")) {
-                ask.add(s);
+                ask.add(s);    /**
+                * Renvoie true si le teenager n'a pas d'habitude l'alimentaire contraire à celui donné en paramètre 
+                * @param necessary un Teenager
+                */
             }
         }
         if(!this.requierments.get("HOST_FOOD").equals("")){
@@ -119,6 +177,7 @@ public class Teenager{
 
     }
 
+
     public boolean isIn(String necessary){
         ArrayList<String> necessaries = new ArrayList<>();
         ArrayList<String> values = new ArrayList<>();
@@ -126,7 +185,7 @@ public class Teenager{
         for (String s : necessary.replaceAll("\\s+","").split(",")) {
             necessaries.add(s);
         }
-        for (String s : this.value.replaceAll("\\s+","").split(",")) {
+        for (String s : this.values.replaceAll("\\s+","").split(",")) {
             values.add(s);
         }
 
